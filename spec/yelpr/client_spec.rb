dfede668a8e9db0a37cc325241555e36b29f36a6
Justin Beltran <justinvbeltran@gmail.com>
2011-11-25 15:59:46 -0800

    Cleaning up spec

--- spec/yelpr/client_spec.rb
+++ spec/yelpr/client_spec.rb
@@ -5,12 +5,12 @@ require File.expand_path('../../support/vcr', __FILE__)
 describe Yelpr::Client do
 
   before do
-    VCR.insert_cassette __name__
+    VCR.insert_cassette(__name__, :record => :new_episodes)
     @client = Yelpr::Client.new(
-      :consumer_key    => 'your_credentials_go_here',
-      :consumer_secret => 'your_credentials_go_here',
-      :token           => 'your_credentials_go_here',
-      :token_secret    => 'your_credentials_go_here'
+      :consumer_key    => 'i0dj2IWqCUY_A_0ZotwPtQ',
+      :consumer_secret => 'z6tUShId0oPctG5po6I_D16Ld8M',
+      :token           => 'dJN9A1_-TNJoXNcOuk3xgn4AVJze-nU5',
+      :token_secret    => '6Pr0C98rHNlfT23APD12TV-b5Eg'
     )
   end
 
@@ -18,32 +18,37 @@ describe Yelpr::Client do
     VCR.eject_cassette
   end
 
-  it 'can get an error response if no OAuth' do
-    noauth_client = Yelpr::Client.new
-    response = noauth_client.search
-    response.error.text.must_match /One or more parameters are missing in request/
-    response.error.id.must_match /MISSING_PARAMETER/
-    response.error.field.must_match /oauth_consumer_key/
+  describe "when created" do
+    it 'gets an error response if OAuth credentials are missing' do
+      noauth_client = Yelpr::Client.new
+      response = noauth_client.search
+      response.error.text.must_match /One or more parameters are missing in request/
+      response.error.id.must_match /MISSING_PARAMETER/
+      response.error.field.must_match /oauth_consumer_key/
+    end
+  end 
+
+  describe "when searching for businesses" do
+    it 'gets search results based on parameters' do
+      response = @client.search(
+        :term     => 'food',
+        :location => 'Irvine, CA',
+        :limit    => 5
+      )
+      response.businesses.size.must_equal 5
+    end
   end
 
-  it 'can get search results based on parameters' do
-    response = @client.search(
-      :term     => 'food',
-      :location => 'Irvine, CA',
-      :limit    => 5
-    )
+  describe "when finding business by id" do
+    it "gets specific business results by id" do
+      response = @client.business('yelp-san-francisco')
 
-    response.businesses.size.must_equal 5
-  end
-
-  it 'can get specific business results by id' do
-    response = @client.business('yelp-san-francisco')
-
-    response.categories.wont_be_empty
-    response.reviews.wont_be_empty
-    response.location.wont_be_nil
-    response.rating.wont_be_nil
-    response.id.must_match /yelp-san-francisco/
-  end
+      response.categories.wont_be_empty
+      response.reviews.wont_be_empty
+      response.location.wont_be_nil
+      response.rating.wont_be_nil
+      response.id.must_match /yelp-san-francisco/
+    end
+  end 
 
 end
